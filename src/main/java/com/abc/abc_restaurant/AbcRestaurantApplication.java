ff9870c755693661c2938a1ed8cb3daafa759afe
sanlk21 <sandunlakmal2001@gmail.com>
2024-08-13 11:42:24 +0530

    first commit

---
+++ src/main/java/com/abc/abc_restaurant/AbcRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.abc.abc_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abc/abc_restaurant/AbcRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.abc_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
