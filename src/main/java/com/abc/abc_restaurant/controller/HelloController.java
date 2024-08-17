455f07772e5c69ba68e59e71cd2fdbc48b3c4e75
sanlk21 <sandunlakmal2001@gmail.com>
2024-08-17 11:10:31 +0530

    update1

--- src/main/java/com/abc/abc_restaurant/controller/HelloController.java
+++ src/main/java/com/abc/abc_restaurant/controller/HelloController.java
@@ -1,9 +1,12 @@
-package com.controller;
+package com.abc.abc_restaurant.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello Spring Boot";
@@ -13,5 +16,5 @@ public class HelloController {
     public String postRequest() {
         return "This is a POST request to Hello endpoint";
     }
-    
+
 }
